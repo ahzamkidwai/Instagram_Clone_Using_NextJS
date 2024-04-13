faaa9e47915db341cc629432afce7ac1d3014041
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-04-13 12:37:28 +0530

    Adding Modal functionality using react-modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { signIn, useSession, signOut } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -13,11 +13,15 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Modal from "react-modal";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   const { data: session } = useSession();
-  // console.log("Session is : ", session);
-  
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className=" shadow-sm border-b stciky top-0 bg-white z-30 p-3">
       <div className="flex items-center justify-between max-w-6xl mx-auto">
@@ -45,12 +49,18 @@ const Header = () => {
         {session ? (
           <div>
             <DropdownMenu>
-              <DropdownMenuTrigger className="text-orange-500">
-                <Avatar>
-                  <AvatarImage src={session.user.image} />
-                  <AvatarFallback>{session.user.name}</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
+              <div className="flex flex-row items-center gap-4">
+                <IoIosAddCircleOutline
+                  className="text-3xl cursor-pointer"
+                  onClick={() => setIsOpen(true)}
+                />
+                <DropdownMenuTrigger className="text-orange-500">
+                  <Avatar>
+                    <AvatarImage src={session.user.image} />
+                    <AvatarFallback>{session.user.name}</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+              </div>
               <DropdownMenuContent>
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                 <DropdownMenuSeparator />
@@ -72,6 +82,34 @@ const Header = () => {
           </button>
         )}
       </div>
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2  rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <HiCamera className="text-gray-400 text-6xl cursor-pointer" />
+            <input
+              type="text"
+              maxLength={150}
+              placeholder="Enter Caption..."
+              className="m-4  text-center w-full focus:ring-0 border-none outline-none"
+            />
+          </div>
+          <button
+            disabled
+            className="w-full bg-red-500 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+          >
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className="cursor-pointer absolute top-2 right-2 hover:text-gray-900 transition duration-300"
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
     </div>
   );
 };
